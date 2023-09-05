8fec23d8d0777ea021a7dc054282f2d837dd862f
dwdsigned <137736096+dwdsigned@users.noreply.github.com>
2023-09-05 11:24:06 -0400

    Update Conditionals.js
    Added switch example from Udacity

--- Conditionals.js
+++ Conditionals.js
@@ -56,3 +56,26 @@ category = eatsAnimals && eatsPlants
 	    : eatsAnimals 
               ? "carnivore" 
               : "undefined";
+// Example of a switch from Udacity
+const option = 3;
+
+switch (option) {
+  case 1:
+    console.log("You selected option 1.");
+    break;
+  case 2:
+    console.log("You selected option 2.");
+    break;
+  case 3:
+    console.log("You selected option 3.");
+    break;
+  case 4:
+    console.log("You selected option 4.");
+    break;
+  case 5:
+    console.log("You selected option 5.");
+    break;
+  case 6:
+    console.log("You selected option 6.");
+    break; // technically, not needed
+}
