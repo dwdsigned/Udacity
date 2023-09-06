e89d6da99bb4e867cd64155d4b52993d68c16350
dwdsigned <137736096+dwdsigned@users.noreply.github.com>
2023-09-06 11:21:20 -0400

    Update Conditionals.js
    garbage file

--- Conditionals.js
+++ Conditionals.js
@@ -79,3 +79,52 @@ switch (option) {
     console.log("You selected option 6.");
     break; // technically, not needed
 }
+
+//#1 change this function into a ternary and assign it to variable called experiencePoints
+function experiencePoints() {
+    if (winBattle()) {
+        return 10;
+    } else {
+        return 1;
+    }
+}
+
+experiencePoints() = winBattle() ? return (10 : 1);
+
+//Using this function, answer the questions below:
+function moveCommand(direction) {
+    var whatHappens;
+    switch (direction) {
+        case "forward":
+            break;
+            whatHappens = "you encounter a monster";
+        case "back":
+            whatHappens = "you arrived home";
+            break;
+            break;
+        case "right":
+            return whatHappens = "you found a river";
+            break;
+        case "left":
+            break;
+            whatHappens = "you run into a troll";
+            break;
+        default:
+            whatHappens = "please enter a valid direction";
+    }
+    return whatHappens;
+}
+
+//#2 return value when moveCommand("forward");
+// nothing; case "forward"
+
+//#3 return value when moveCommand("back");
+// returns case "back" and "you arrived home"
+
+//#4 return value when moveCommand("right");
+// case "right" and "you found a river"
+
+//#5 return value when moveCommand("left");
+// nothing; case "left"
+
+//BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
